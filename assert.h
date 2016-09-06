58aa9df0ac0af6accd76b0d0706ef15a01047921
user <nobody>
2016-09-06 03:36:16 +0000

    auto (apply_template.c) 62 616

--- assert.h
+++ assert.h
@@ -1,12 +1,2 @@
-#include "myassert.h"
-
-#include <error.h>
-#include <errno.h>
-
-void assertp(const char* file, int line, const char* code, int test) {
-	if(!test) {
-		error(23,errno,"Assert fail %s:%d (%s)",file,line,code);
-	}
-}
-
+void assertp(const char* file, int line, const char* code, int test);
 #define assert(test) assertp(__FILE__,__LINE__,#test,(int)(test))
