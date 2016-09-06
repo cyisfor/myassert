5b6f184988c53c7383fe00265cf899207cbdd432
user <nobody>
2016-09-06 03:35:57 +0000

    auto (myassert.h) 58 530

---
+++ module.c
@@ -0,0 +1,12 @@
+#include "assert.h"
+
+#include <error.h>
+#include <errno.h>
+
+void assertp(const char* file, int line, const char* code, int test) {
+	if(!test) {
+		error(23,errno,"Assert fail %s:%d (%s)",file,line,code);
+	}
+}
+
+#define assert(test) assertp(__FILE__,__LINE__,#test,(int)(test))
